ee7b45f6f57506eb3c4661cce1c2ad769a4f5ee6
Arsen Petrov <pbmspa@gmail.com>
2022-07-16 19:40:09 +0300

    the fourth attemption

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/telegram-web-app/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: 'https://keet5.github.io/telegram-web-app/',
 })
